61791490897bb21605f16755d20372450a77fa78
Eugen Schneider <schneider.eugen@hotmail.com>
2025-05-11 21:35:46 +0300

    preset: netlify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,6 +24,7 @@ export default defineNuxtConfig({
 
   // https://nuxt.com/docs/api/nuxt-config#nitro
   nitro: {
+    preset: 'netlify',
     experimental: {
       database: true,
       tasks: true,
